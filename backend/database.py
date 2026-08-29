80c110028627921e5ca8e7aa28cf88bc95738195
hardeepsinh11 <jadejahardeepsinh81@gmail.com>
2026-08-29 16:41:35 +0530

    deploy

--- backend/database.py
+++ backend/database.py
@@ -7,8 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # 1. Engine 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
