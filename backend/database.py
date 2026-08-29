b0676607f366c7f144d41dd179adf9bfa1bd4cb5
hardeepsinh11 <jadejahardeepsinh81@gmail.com>
2026-08-29 14:10:57 +0530

    Fix: Updated index.html script src path

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "sqlite:///./megastore.db"
-
-# 1. Engine બનાવો
+DATABASE_URL = os.getenv("DATABASE_URL")
+# 1. Engine 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-# 2. Session બનાવો (વાતચીત કરવા માટે)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# 3. Base (જેના પરથી models બનશે)
 Base = declarative_base()
 
 # 4. Dependency (દરેક API માં કામ લાગશે)
