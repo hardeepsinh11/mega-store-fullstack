cd06f75c60d883bd64eb3d1756745913e8b19b62
hardeepsinh11 <jadejahardeepsinh81@gmail.com>
2026-08-29 16:25:30 +0530

    deploy

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.orm import sessionmaker,declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 # 1. Engine 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
