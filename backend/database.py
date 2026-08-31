d8638caabc64cab8994b2cfdaf1d4e79755a0ada
hardeepsinh11 <jadejahardeepsinh81@gmail.com>
2026-08-31 17:43:14 +0530

    databsechanges

--- backend/database.py
+++ backend/database.py
@@ -1,21 +1,15 @@
 from sqlalchemy import create_engine
-
 from sqlalchemy.orm import sessionmaker,declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL is not set")
-
 print("DATABASE URL:", DATABASE_URL.split("@")[-1])
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
-# 4. Dependency (દરેક API માં કામ લાગશે)
 def get_db():
     db = SessionLocal()
     try:
